94fe6ead94c7a43a675aed09660a926bab5e51ba
FoxyFeisty <36619363+FoxyFeisty@users.noreply.github.com>
2018-07-27 08:31:35 +0200

    Add files via upload
    Filtering links added

--- sendURL.php
+++ sendURL.php
@@ -15,7 +15,8 @@ if ($xml !== false) {
 	preg_match("/<body[^>]*>(.*?)<\/body>/is", $xml, $matched);
 	$filter_body = preg_replace("/<body[^>]*>/is", "", $matched[0]);
 	$filter_body = preg_replace("/<\/body>/is", "", $filter_body);
-	$filter_script = preg_replace("/<script[^>]*>(.*?)<\/script>/is", "", $filter_body);
+	$filter_link = preg_replace("/<a[^>]*>(.*?)<\/a>/is", "", $filter_body);
+	$filter_script = preg_replace("/<script[^>]*>(.*?)<\/script>/is", "", $filter_link);
 	$filter_br = preg_replace("/(<br[^>]*>\s*){2,}/is", "", $filter_script);
 	$filter_all = preg_replace("/<img[^>]*>/is", "", $filter_br);
 
